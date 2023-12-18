f0fd7fb532faa3ad54da07128cdf245a7c7cbe57
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-18 09:01:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function convertToRoman(num) {
 
 		num %= value;
 	}
+	result = result.replace('IIII', 'IV').replace('VIV', 'IX').replace('XXXX', 'XL').replace('LXL', 'XC');
 
 	return ans;
 	
@@ -29,7 +30,7 @@ function convertToRoman(num) {
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
